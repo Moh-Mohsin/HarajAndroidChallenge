cf891c4ada14bccc7c3534cb2792d075c377043e
Moh-Mohsin <52405631+Moh-Mohsin@users.noreply.github.com>
2021-07-04 03:36:28 +0200

    add share option to post detail

--- app/src/main/java/com/example/harajtask/ui/postdetail/PostDetailFragment.kt
+++ app/src/main/java/com/example/harajtask/ui/postdetail/PostDetailFragment.kt
@@ -3,14 +3,14 @@ package com.example.harajtask.ui.postdetail
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import coil.api.load
 import com.example.harajtask.R
 import com.example.harajtask.databinding.PostDetailFragmentBinding
-import com.example.harajtask.databinding.PostListFragmentBinding
 import com.example.harajtask.ui.postlist.format
+import com.example.harajtask.util.shareText
 import com.example.harajtask.util.viewBinding
 
 class PostDetailFragment : Fragment(R.layout.post_detail_fragment) {
@@ -29,6 +29,21 @@ class PostDetailFragment : Fragment(R.layout.post_detail_fragment) {
         binding.username.text = post.username
         binding.city.text = post.city
         binding.body.text = post.body
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.post_detail_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_share -> {
+                shareText(post.title)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
--- app/src/main/java/com/example/harajtask/util/UiExt.kt
+++ app/src/main/java/com/example/harajtask/util/UiExt.kt
@@ -55,3 +55,14 @@ fun View.hide(){
     visibility = View.GONE
 }
 
+fun Fragment.shareText(textToShare: String) {
+    val sendIntent: Intent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_TEXT, textToShare)
+        type = "text/plain"
+    }
+    val shareIntent = Intent.createChooser(sendIntent, null)
+    startActivity(shareIntent)
+
+}
+
